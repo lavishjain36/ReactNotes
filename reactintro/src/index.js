d8a49d2205a1a6b9114fa25c458f4a056fee3df0
Lavish Jain <lavishjain36@gmail.com>
2021-01-22 22:18:59 +0530

    reactintro

---
+++ reactintro/src/index.js
@@ -0,0 +1,13 @@
+// var React = require("react");
+// var ReactDOM = require("react-dom");
+import ReactDOM from "react-dom";
+
+import React from "react";
+
+ReactDOM.render(
+  <h1>Hello People. This is Lavish.</h1>,
+
+  document.getElementById("root")
+);
+
+// The content which is written here is not the html.This is JSX
